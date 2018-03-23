976923a145e46cd577b8f4d0a887c76b9cbb5cbb
srivinod <vinodsemailbox@gmail.com>
2018-03-23 15:58:11 +0530

    Build ready

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,35 @@
 import { Component } from '@angular/core';
+import { Article } from './article/article.model';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent { 
+  articles: Article[];
+  
+  constructor(){
+    this.articles = [
+      new Article('Angular 5', 'http://angular.io', 3),
+      new Article('Github', 'https://github.com/', 2),
+      new Article('React JS', 'https://reactjs.org/', 11)
+    ];
+  }
+
+  addArticle(title: HTMLInputElement, link: HTMLInputElement): boolean {
+    // Note how `(backtick) single allows angular variables below
+    console.log(`Adding title: ${title.value} and link :${link.value}`);
+    this.articles.push(new Article(title.value, link.value, 0));
+    title.value = '';
+    link.value = '';
+    return false;
+  }
+
+  sortedArticles(): Article[]{ // Sort ascending based on vote points
+    return this.articles.sort((a: Article, b: Article) => b.votes - a.votes);
+  }
+
+
+
 }
---
+++ src/app/article/article.component.ts
@@ -0,0 +1,32 @@
+import { Component, OnInit, Input } from '@angular/core';
+import {  Article } from './article.model';
+
+@Component({
+  selector: 'app-article',
+  templateUrl: './article.component.html',
+  styleUrls: ['./article.component.css']
+})
+export class ArticleComponent implements OnInit {
+  votes:  number;
+  title:  string;
+  link: string; 
+  @Input() article : Article;
+
+  constructor() {
+
+  }
+
+   voteUp():  boolean{
+     this.article.voteUp();
+     return false;
+   }
+
+   voteDown():  boolean{
+     this.article.voteDown();
+     return false;
+   }
+
+  ngOnInit() {
+  }
+
+}
